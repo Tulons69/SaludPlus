3ba53dd8be6acebf03198177786c3b86ec26dccb
Diego.Gonzalez <die.gonzalezh@duocuc.cl>
2026-06-26 05:42:05 -0400

    Update usuario_setup.sql

--- SaludPlus/SaludPlus_V2_Final (1)/SaludPlus_V2/SaludPlus-Final/sql/usuario_setup.sql
+++ SaludPlus/SaludPlus_V2_Final (1)/SaludPlus_V2/SaludPlus-Final/sql/usuario_setup.sql
@@ -1,22 +1,13 @@
--- ============================================================
--- SCRIPT COMPLETO PARA MICROSERVICIO: usuario
--- Según lineamientos del documento de Migración de BD
--- ============================================================
-
-/* CREACION DE TABLA ORIGINAL */
 CREATE TABLE IF NOT EXISTS usuario (
     id     INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(20),
     edad   INT
 );
 
-/* INSERCION DE DATOS - Hacerlo con Postman método POST */
 INSERT INTO usuario (id, nombre, edad) VALUES (3, 'Marco', 25);
 
-/* CONSULTA DE DATOS - Hacerlo con Postman método GET */
 SELECT * FROM usuario_backup;
 
-/* CREAR TABLA BACKUP PARA EL MICROSERVICIO */
 CREATE TABLE IF NOT EXISTS usuario_backup (
     id        INT,
     nombre    VARCHAR(20),
@@ -24,8 +15,6 @@ CREATE TABLE IF NOT EXISTS usuario_backup (
     backup_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-/* CREAR TRIGGER PARA EL MICROSERVICIO */
-/* Con esto automáticamente se guardarán los datos en el backup */
 DELIMITER //
 CREATE TRIGGER despues_insert_usuario
 AFTER INSERT ON usuario
