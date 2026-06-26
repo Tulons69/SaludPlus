e9210c0e2ab8d19e51b384326040b11507ea3c78
Diego.Gonzalez <die.gonzalezh@duocuc.cl>
2026-06-26 05:48:40 -0400

    Update V1__create_doctores_table.sql

--- SaludPlus/SaludPlus_V2_Final (1)/SaludPlus_V2/doctor-service/target/classes/db/migration/V1__create_doctores_table.sql
+++ SaludPlus/SaludPlus_V2_Final (1)/SaludPlus_V2/doctor-service/target/classes/db/migration/V1__create_doctores_table.sql
@@ -1,6 +1,3 @@
--- V1__create_doctores_table.sql
--- Migracion inicial: creacion de la tabla doctores
-
 CREATE TABLE IF NOT EXISTS doctores (
     id          INT AUTO_INCREMENT PRIMARY KEY,
     rut         VARCHAR(12)  NOT NULL,
